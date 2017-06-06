2ab3153ab7f1356883f5095a4f525f2797c04219
pawel <karczewskipawel93@gmail.com>
2017-06-06 23:09:30 +0200

    Added new project: TerminalInterface

---
+++ src/TerminalInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TerminalInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
