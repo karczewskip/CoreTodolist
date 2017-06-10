d5c4555115488bf2acd2094259e089928926934d
pawel <karczewskipawel93@gmail.com>
2017-06-10 18:58:39 +0200

    Added TodoTask entity

---
+++ src/CoreTodoList.DAL/Entities/TodoTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreTodoList.DAL.Entities
+{
+    public class TodoTask
+    {
+		public int TodoTaskId { get; set; }
+		public string Title { get; set; }
+	}
+}
