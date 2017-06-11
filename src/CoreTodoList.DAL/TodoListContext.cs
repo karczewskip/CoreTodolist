8c95acbffc2da891724ac7cb80e4b9654c62a86e
pawel <karczewskipawel93@gmail.com>
2017-06-11 17:16:18 +0200

    Added DbContext to DAL project.

---
+++ src/CoreTodoList.DAL/TodoListContext.cs
@@ -0,0 +1,18 @@
+﻿using CoreTodoList.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreTodoList.DAL
+{
+    public class TodoListContext : DbContext
+    {
+		public TodoListContext(DbContextOptions<TodoListContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<TodoTask> TodoTasks { get; set; }
+    }
+}
