4b108cf4f5ed63e003458bc330c25dee9e2898bf
pawel <karczewskipawel93@gmail.com>
2017-06-15 14:13:34 +0200

    Set Title in TodoTasks as required field.

--- src/CoreTodoList.DAL/Entities/TodoTask.cs
+++ src/CoreTodoList.DAL/Entities/TodoTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CoreTodoList.DAL.Entities
@@ -7,6 +8,8 @@ namespace CoreTodoList.DAL.Entities
     public class TodoTask
     {
 		public int TodoTaskId { get; set; }
+
+		[Required]
 		public string Title { get; set; }
 	}
 }
