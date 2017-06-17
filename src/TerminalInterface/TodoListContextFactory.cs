d82b6ea373c5bbb584eb1ab9a431eb5c2ac01439
pawel <karczewskipawel93@gmail.com>
2017-06-17 09:09:26 +0200

    Created correct TodoListContextFactory in TerminalInterface instead of in DAL project.

---
+++ src/TerminalInterface/TodoListContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using CoreTodoList.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace TerminalInterface
+{
+	public class TodoListContextFactory : IDbContextFactory<TodoListContext>
+	{
+		public TodoListContext Create(DbContextFactoryOptions options)
+		{
+			var configuration = new ConfigurationBuilder()
+			 .SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json").Build();
+
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+			var contextOptions = new DbContextOptionsBuilder<TodoListContext>().UseSqlServer(connectionString).Options;
+			return new TodoListContext(contextOptions);
+		}
+	}
+}
